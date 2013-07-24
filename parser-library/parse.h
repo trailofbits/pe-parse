58643abf49e40e0426b0bc684db0c34683b7b204
Andrew <munin@mimisbrunnr.net>
2013-07-24 17:35:25 -0400

    .

--- parser-library/parse.h
+++ parser-library/parse.h
@@ -14,6 +14,19 @@ typedef struct _parsed_pe {
 //get a PE parse context from a file 
 parsed_pe *ParsePEFromFile(const char *filePath);
 
+//destruct a PE context
 void DestructParsedPE(parsed_pe *pe);
+
+//iterate over the imports by RVA and string 
+typedef void (*iterRVAStr)(void *, RVA, std::string &);
+void IterImpRVAString(parsed_pe *pe, iterRVAStr cb, void *cbd);
+
+//iterate over relocations in the PE file
+typedef void (*iterReloc)(void *, RVA);
+void IterRelocs(parsed_pe *pe, iterReloc cb, void *cbd);
+
+//iterate over the exports by RVA
+typedef void (*iterRVA)(void *, RVA);
+void IterExpRVA(parsed_pe *pe, iterRVA cb, void *cbd);
 
 #endif
