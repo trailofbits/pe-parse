32da0651edcfc3b0262c295fa242795cde37fc8f
Andrew <munin@mimisbrunnr.net>
2013-07-26 09:42:14 -0400

    dumping code and fields

--- dump-prog/dump.cpp
+++ dump-prog/dump.cpp
@@ -54,19 +54,49 @@ int main(int argc, char *argv[]) {
     if(p != NULL) {
       //print out some things
 #define DUMP_FIELD(x) \
-      cout << "" #x << ": "; \
+      cout << "" #x << ": 0x"; \
       cout << to_string<uint32_t>(p->peHeader.x, hex) << endl;
+#define DUMP_DEC_FIELD(x) \
+      cout << "" #x << ": "; \
+      cout << to_string<uint32_t>(p->peHeader.x, dec) << endl;
 
       DUMP_FIELD(nt.Signature);
       DUMP_FIELD(nt.FileHeader.Machine);
       DUMP_FIELD(nt.FileHeader.NumberOfSections);
-      DUMP_FIELD(nt.FileHeader.TimeDateStamp);
+      DUMP_DEC_FIELD(nt.FileHeader.TimeDateStamp);
       DUMP_FIELD(nt.FileHeader.PointerToSymbolTable);
-      DUMP_FIELD(nt.FileHeader.NumberOfSymbols);
+      DUMP_DEC_FIELD(nt.FileHeader.NumberOfSymbols);
       DUMP_FIELD(nt.FileHeader.SizeOfOptionalHeader);
       DUMP_FIELD(nt.FileHeader.Characteristics);
-     
+      DUMP_FIELD(nt.OptionalHeader.Magic);
+      DUMP_DEC_FIELD(nt.OptionalHeader.MajorLinkerVersion);
+      DUMP_DEC_FIELD(nt.OptionalHeader.MinorLinkerVersion);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfCode);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfInitializedData);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfUninitializedData);
+      DUMP_FIELD(nt.OptionalHeader.AddressOfEntryPoint);
+      DUMP_FIELD(nt.OptionalHeader.BaseOfCode);
+      DUMP_FIELD(nt.OptionalHeader.BaseOfData);
+      DUMP_FIELD(nt.OptionalHeader.ImageBase);
+      DUMP_FIELD(nt.OptionalHeader.SectionAlignment);
+      DUMP_FIELD(nt.OptionalHeader.FileAlignment);
+      DUMP_DEC_FIELD(nt.OptionalHeader.MajorOperatingSystemVersion);
+      DUMP_DEC_FIELD(nt.OptionalHeader.MinorOperatingSystemVersion);
+      DUMP_DEC_FIELD(nt.OptionalHeader.Win32VersionValue);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfImage);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfHeaders);
+      DUMP_FIELD(nt.OptionalHeader.CheckSum);
+      DUMP_FIELD(nt.OptionalHeader.Subsystem);
+      DUMP_FIELD(nt.OptionalHeader.DllCharacteristics);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfStackReserve);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfStackCommit);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfHeapReserve);
+      DUMP_FIELD(nt.OptionalHeader.SizeOfHeapCommit);
+      DUMP_FIELD(nt.OptionalHeader.LoaderFlags);
+      DUMP_DEC_FIELD(nt.OptionalHeader.NumberOfRvaAndSizes);
+
 #undef DUMP_FIELD
+#undef DUMP_DEC_FIELD
 
       IterImpRVAString(p, printImports, NULL);
       IterRelocs(p, printRelocs, NULL);
