3c4d128efdb82afc04402ad2b3125a215eb4545e
Tony Theodore <tonyt@logyst.com>
2018-03-26 18:31:58 +1100

    add option to build shared libs

--- pe-parser-library/cmake/peparse-config.cmake
+++ pe-parser-library/cmake/peparse-config.cmake
@@ -1,5 +1,5 @@
 find_path(PEPARSE_INCLUDE_DIR "parser-library/parse.h")
-find_library(PEPARSE_LIBRARIES NAMES "libpe-parser-library.a")
+find_library(PEPARSE_LIBRARIES NAMES "libpe-parser-library")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libproxy DEFAULT_MSG PEPARSE_INCLUDE_DIR PEPARSE_LIBRARIES)
+find_package_handle_standard_args(peparse DEFAULT_MSG PEPARSE_INCLUDE_DIR PEPARSE_LIBRARIES)
