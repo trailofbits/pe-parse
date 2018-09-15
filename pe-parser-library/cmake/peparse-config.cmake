57981d713792761833358a09b04495b6143ef4b8
Jack Williams <jacwil014@gmail.com>
2018-09-15 02:40:40 -0700

    Fix cmake config for pre-Windows 10 installations

--- pe-parser-library/cmake/peparse-config.cmake
+++ pe-parser-library/cmake/peparse-config.cmake
@@ -1,5 +1,5 @@
-find_path(PEPARSE_INCLUDE_DIR "parser-library/parse.h")
-find_library(PEPARSE_LIBRARIES NAMES "libpe-parser-library")
+find_path(PEPARSE_INCLUDE_DIR $<SHELL_PATH:"parser-library/parse.h">)
+find_library(PEPARSE_LIBRARIES NAMES "libpe-parser-library" "pe-parser-library")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(peparse DEFAULT_MSG PEPARSE_INCLUDE_DIR PEPARSE_LIBRARIES)
