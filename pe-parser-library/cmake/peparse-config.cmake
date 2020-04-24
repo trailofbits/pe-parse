79a2333a8b1175777f438e0c102744751dc4abdf
William Woodruff <william.woodruff@trailofbits.com>
2020-04-24 13:48:46 -0400

    treewide: CI, cmake fixes (#132)
    Massages the pe-parse build into a format that's more accomodating
    for vcpkg, in preparation for imminent packaging.

--- pe-parser-library/cmake/peparse-config.cmake
+++
@@ -1,9 +0,0 @@
-if(CMAKE_CROSSCOMPILING)
-  find_path(PEPARSE_INCLUDE_DIR "parser-library/parse.h")
-else()
-  find_path(PEPARSE_INCLUDE_DIR $<SHELL_PATH:"parser-library/parse.h">)
-endif()
-find_library(PEPARSE_LIBRARIES NAMES "libpe-parser-library" "pe-parser-library")
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(peparse DEFAULT_MSG PEPARSE_INCLUDE_DIR PEPARSE_LIBRARIES)
