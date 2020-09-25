327a524de1a32000ffd5110b024d09ebef22aae0
passthecilantro <65034117+passthecilantro@users.noreply.github.com>
2020-09-25 15:48:28 -0400

    Fix #1115 (#139)
    Co-authored-by: William Woodruff <william.woodruff@trailofbits.com>
    Co-authored-by: William Woodruff <william@trailofbits.com>

--- dump-pe/main.cpp
+++ dump-pe/main.cpp
@@ -29,6 +29,8 @@ THE SOFTWARE.
 
 #include <pe-parse/parse.h>
 
+#include "vendor/argh.h"
+
 using namespace peparse;
 
 int printExps(void *N,
@@ -295,14 +297,21 @@ int printSecs(void *N,
   std::cout << std::boolalpha << static_cast<bool>(p->peHeader.x) << "\n";
 
 int main(int argc, char *argv[]) {
-  if (argc != 2 || (argc == 2 && std::strcmp(argv[1], "--help") == 0)) {
+
+  argh::parser cmdl(argv);
+
+  if (cmdl[{"-h", "--help"}] || argc <= 1) {
     std::cout << "dump-pe utility from Trail of Bits\n";
     std::cout << "Repository: https://github.com/trailofbits/pe-parse\n\n";
     std::cout << "Usage:\n\tdump-pe /path/to/executable.exe\n";
-    return 1;
+    return 0;
+  } else if (cmdl[{"-v", "--version"}]) {
+    std::cout << "dump-pe (pe-parse) version " << PEPARSE_VERSION << "\n";
+    return 0;
   }
 
-  parsed_pe *p = ParsePEFromFile(argv[1]);
+  parsed_pe *p = ParsePEFromFile(cmdl[1].c_str());
+
   if (p == nullptr) {
     std::cout << "Error: " << GetPEErr() << " (" << GetPEErrString() << ")"
               << "\n";
