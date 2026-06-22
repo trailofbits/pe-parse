a186e0bad11baebd78d44afce4f42a0350530f48
Dan Guido <dan@trailofbits.com>
2026-06-22 10:08:06 -0400

    Make warnings-as-errors opt-in

--- cmake/compilation_flags.cmake
+++ cmake/compilation_flags.cmake
@@ -12,7 +12,7 @@ if (MSVC)
     list(APPEND DEFAULT_CXX_FLAGS /Zi)
   endif ()
 
-  if (CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
+  if (PEPARSE_WARNINGS_AS_ERRORS)
     list(APPEND DEFAULT_CXX_FLAGS /WX)
   endif ()
 
@@ -26,19 +26,23 @@ else ()
     -pedantic -Wall -Wextra -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization
     -Wformat=2 -Winit-self -Wlong-long -Wmissing-declarations -Wmissing-include-dirs -Wcomment
     -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow -Wsign-conversion
-    -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Werror -Wunused -Wuninitialized
+    -Wsign-promo -Wstrict-overflow=5 -Wswitch-default -Wundef -Wunused -Wuninitialized
     -Wno-missing-declarations -Wno-strict-overflow
   )
 
+  if (PEPARSE_WARNINGS_AS_ERRORS)
+    list(APPEND DEFAULT_CXX_FLAGS -Werror)
+  endif ()
+
   if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
-  list(APPEND DEFAULT_CXX_FLAGS -gdwarf-2 -g3)
+    list(APPEND DEFAULT_CXX_FLAGS -gdwarf-2 -g3)
   endif ()
 
   if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     list(APPEND DEFAULT_CXX_FLAGS
       -Wno-c++98-compat -Wno-missing-prototypes
       -Wno-missing-variable-declarations -Wno-global-constructors
-      -Wno-exit-time-destructors -Wno-padded -Wno-error
+      -Wno-exit-time-destructors -Wno-padded
     )
   endif ()
 endif ()
